652f891185c98d1029eb3937d2f1a9b60f7041a7
Fabian Pfuetsch <fabian.pfuetsch@volkswagen.de>
2019-08-30 13:31:23 +0200

    Refactor and style.

--- sketch.js
+++ sketch.js
@@ -6,19 +6,23 @@ class Point {
 }
 
 const maxLengthSlider = document.getElementById('maxLengthSlider');
-const minLengthSlider = document.getElementById('minLengthSlider');
+const recursionLevelSlider = document.getElementById('recursionLevelSlider');
 const splitAngleSlider = document.getElementById('splitAngleSlider');
-const shrinkFactorSlider = document.getElementById('shrinkFactorSlider');
+const growFactorSlider = document.getElementById('growFactorSlider');
 const thiccSlider = document.getElementById('thiccSlider');
 const sliders = Array.from(document.getElementsByClassName('slider'));
 const growBtn = document.getElementById('growBtn');
 
+const calcMinBranchLength = (maxLength, grow, level) => {
+    return maxLength * Math.pow(grow, level);
+}
+
 const getConfiguration = () => {
     return {
-        minBranchLength: parseInt(minLengthSlider.value),
+        minBranchLength: calcMinBranchLength(parseInt(maxLengthSlider.value), parseInt(growFactorSlider.value) / 100, parseInt(recursionLevelSlider.value)),
         maxBranchLength: parseInt(maxLengthSlider.value),
         splitAngle: parseInt(splitAngleSlider.value),
-        shrinkFactor: parseInt(shrinkFactorSlider.value) / 100,
+        growFactor: parseInt(growFactorSlider.value) / 100,
         thiccFactor: parseInt(thiccSlider.value)
     }
 }
@@ -49,26 +53,31 @@ const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-const growTree = async () => {
+const growTree = async () => {  
     sliders.forEach(slider => slider.disabled = true);
     growBtn.disabled = true;
+
     const targetMaxLength = config.maxBranchLength;
     const targetTHICCness = config.thiccFactor;
     const targetAngle = config.splitAngle - 5;
-    const lengthDifferenceFactor = config.minBranchLength / config.maxBranchLength;
+    const targetLevel = recursionLevelSlider.value;
+
     const growCycles = 100;
-    let currentMaxLength = 0, currentTHICCness = 0, currentAngle = 0;
+    let currentMaxLength = 0, currentTHICCness = 0, currentAngle = 0, currentLevel = 0;
+
     for(let i = 1; i <= growCycles; i++){
         await sleep(5000 / growCycles)
         currentMaxLength = i * targetMaxLength / growCycles;
         currentTHICCness = i * targetTHICCness / growCycles;
         currentAngle = i * targetAngle / growCycles + 5;
+        currentLevel = i * targetLevel / growCycles;
         config.maxBranchLength = currentMaxLength;
         config.thiccFactor = currentTHICCness;
         config.splitAngle = currentAngle;
-        config.minBranchLength = currentMaxLength * lengthDifferenceFactor;
+        config.minBranchLength = calcMinBranchLength(currentMaxLength, config.growFactor, currentLevel)
         drawNewTree();
     }
+
     sliders.forEach(slider => slider.disabled = false);
     growBtn.disabled = false;
 }
@@ -78,11 +87,12 @@ const drawFractal = (start, length, angle) => {
         const nextPoint = calcNextPoint(start, length, angle)
         setDesign(length);
         line(start.x, start.y, nextPoint.x, nextPoint.y);
-        drawFractal(nextPoint, length * config.shrinkFactor, (angle + config.splitAngle) % 360);
+        drawFractal(nextPoint, length * config.growFactor, (angle + config.splitAngle) % 360);
         setDesign(length);
-        drawFractal(nextPoint, length * config.shrinkFactor, (angle - config.splitAngle) % 360);
+        drawFractal(nextPoint, length * config.growFactor, (angle - config.splitAngle) % 360);
     }
 }
+
 const setDesign = (length) => {
     const design = calcDesign(length);
     stroke(design.red, design.green, design.blue);
@@ -93,12 +103,12 @@ const calcNextPoint = (point, length, angle) => {
     angle = angle * (Math.PI / 180);
     return {x: point.x - Math.round(Math.cos(angle)*length),
             y: point.y - Math.round(Math.sin(angle)*length)};
-};
+}
 
 const calcDesign = (length) => {
     const dynamicDiff = (length - config.minBranchLength) / (config.maxBranchLength - config.minBranchLength);
-    const brown = {red: Math.random() * 20 + 155, green: Math.random() * 20 + 32, blue: Math.random() * 20 + 32};
-    const leaf = {red: Math.random() * 20 + 0, green: Math.random() * 20 + 155, blue: 0};
+    const brown = {red: 165, green: 42, blue: 42};
+    const leaf = {red: 10, green: 165, blue: 0};
     return {
         red: (brown.red -  leaf.red) * dynamicDiff + leaf.red,
         green: (brown.green -  leaf.green) * dynamicDiff + leaf.green,
